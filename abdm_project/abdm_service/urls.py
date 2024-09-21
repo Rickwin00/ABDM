b12029fd7b2740b340064d68d8c5b4a2a5021e10
Rickwin00 <rickwin.xavier@onedatasoftware.com>
2024-09-21 16:08:00 +0530

    Views changes

--- abdm_project/abdm_service/urls.py
+++ abdm_project/abdm_service/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from abdm_service.views import ABHARequestOTPView, HealthView
+from abdm_service.views import ABHARequestOTPView, HealthView, ABHASessionView
 
 
 
 urlpatterns = [
     path("health/", HealthView.as_view(), name="health"),
-    path("abha-request-otp/", ABHARequestOTPView.as_view(), name="abha-request-otp")
+    path("abha-request-otp/", ABHARequestOTPView.as_view(), name="abha-request-otp"),
+    path("abha-session/", ABHASessionView.as_view(), name="abha-session")
 ]
